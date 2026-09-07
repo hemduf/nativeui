413e914e6d807e8592a108a612c0362cd666f47d
hemduf <fduhem@gmail.com>
2026-09-07 06:37:56 +0200

    render: expose image resource API

--- include/nativeui/nativeui.hpp
+++ include/nativeui/nativeui.hpp
@@ -9,6 +9,7 @@
 #include <nativeui/text_edit.hpp>
 #include <nativeui/text.hpp>
 #include <nativeui/path.hpp>
+#include <nativeui/image.hpp>
 #include <nativeui/paint.hpp>
 #include <nativeui/component.hpp>
 #include <nativeui/command.hpp>
