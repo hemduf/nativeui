898b5bfccf1113679f395c6d3fb6eb04b246e861
hemduf <fduhem@gmail.com>
2026-09-07 04:17:45 +0200

    text: expose TextArea component

--- include/nativeui/widgets.hpp
+++ include/nativeui/widgets.hpp
@@ -1,4 +1,5 @@
 #pragma once
 #include <nativeui/detail/widgets_basic.inc>
 #include <nativeui/detail/widgets_text_input.inc>
+#include <nativeui/detail/widgets_text_area.inc>
 #include <nativeui/detail/widgets_builders.inc>
