bb5e78e9733e9aea38041ef4ac5e74d8cd83432d
hemduf <fduhem@gmail.com>
2026-09-09 10:34:21 +0200

    fix: include Button after widget namespace closure

--- include/nativeui/widgets.hpp
+++ include/nativeui/widgets.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <nativeui/detail/widgets_basic.inc>
-#include <nativeui/detail/widgets_button.inc>
 #include <nativeui/detail/widgets_text_input.inc>
 #include <nativeui/detail/widgets_text_area.inc>
 #include <nativeui/detail/widgets_builders.inc>
+#include <nativeui/detail/widgets_button.inc>
