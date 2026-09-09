3913a4248952d4720e02d3e9058360dc17bb029f
hemduf <fduhem@gmail.com>
2026-09-09 08:04:52 +0200

    build: isolate T053 consumer registrations

--- cmake/NativeUIConsumerPlatform.cmake
+++ cmake/NativeUIConsumerPlatform.cmake
@@ -37,3 +37,36 @@ function(nativeui_compute_objc_runtime_prefix out_var consumer_id)
       "NUI_${_nativeui_fragment}_${_nativeui_digest12}_"
       PARENT_SCOPE)
 endfunction()
+
+# Configure-time bookkeeping only. These GLOBAL CMake properties disappear
+# after generation; no process/runtime registry is emitted into NativeUI.
+function(_nativeui_register_consumer_identity target consumer_id)
+  if("${target}" STREQUAL "")
+    message(FATAL_ERROR "NativeUI consumer registration requires a target name")
+  endif()
+  if("${consumer_id}" STREQUAL "")
+    message(FATAL_ERROR "NativeUI CONSUMER_ID must be non-empty")
+  endif()
+
+  string(SHA256 _target_key "${target}")
+  string(SHA256 _identity_key "${consumer_id}")
+  set(_target_property "NATIVEUI_T053_TARGET_${_target_key}")
+  set(_identity_property "NATIVEUI_T053_ID_${_identity_key}")
+
+  get_property(_target_is_set GLOBAL PROPERTY "${_target_property}" SET)
+  if(_target_is_set)
+    get_property(_existing_identity GLOBAL PROPERTY "${_target_property}")
+    message(FATAL_ERROR
+      "NativeUI target '${target}' already has consumer identity '${_existing_identity}'; duplicate attachment is not allowed")
+  endif()
+
+  get_property(_identity_is_set GLOBAL PROPERTY "${_identity_property}" SET)
+  if(_identity_is_set)
+    get_property(_existing_target GLOBAL PROPERTY "${_identity_property}")
+    message(FATAL_ERROR
+      "NativeUI CONSUMER_ID '${consumer_id}' is already registered to target '${_existing_target}'; target '${target}' cannot reuse it")
+  endif()
+
+  set_property(GLOBAL PROPERTY "${_target_property}" "${consumer_id}")
+  set_property(GLOBAL PROPERTY "${_identity_property}" "${target}")
+endfunction()
