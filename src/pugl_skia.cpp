3a9476c86917550fdfa1c81485f8443e7706bac0
hemduf <fduhem@gmail.com>
2026-09-09 13:57:06 +0200

    Handle non-fatal standalone raise status

--- src/pugl_skia.cpp
+++ src/pugl_skia.cpp
@@ -1,5 +1,6 @@
 #include "detail/native_ime_bridge.h"
 #include "detail/pugl_skia_setup.inc"
+#include "detail/pugl_skia_show_policy.inc"
 #include "detail/pugl_skia_view_a.inc"
 #include "detail/pugl_skia_view_b.inc"
 #include "detail/pugl_skia_windows.inc"
