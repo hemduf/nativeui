c14b0e84f06ef7e162d8c5ae12b0d564f8b0a747
hemduf <fduhem@gmail.com>
2026-09-10 10:47:27 +0200

    test: expose T058 conditional API through umbrella header

--- tests/headers/nativeui.cpp
+++ tests/headers/nativeui.cpp
@@ -1,3 +1,8 @@
 #include <nativeui/nativeui.hpp>
 
 void nativeui_header_compile_nativeui() {}
+
+void nativeui_header_compile_t058_conditional(ui::State<bool>& visible) {
+    auto spec = ui::make_spec(ui::If{visible, ui::Spacer{1.0f, 1.0f}});
+    (void)spec;
+}
