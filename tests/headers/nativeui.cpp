8c52040fc5d957f2f8558f8556ab1a1147e1fb5c
hemduf <fduhem@gmail.com>
2026-09-10 16:09:19 +0200

    test: wire T045 public semantics RED

--- tests/headers/nativeui.cpp
+++ tests/headers/nativeui.cpp
@@ -1,3 +1,9 @@
 #include <nativeui/nativeui.hpp>
+#include <nativeui/semantics.hpp>
 
-void nativeui_header_compile_nativeui() {}
+void nativeui_header_compile_nativeui() {
+    ui::SemanticInfo info;
+    info.role = ui::SemanticRole::Custom;
+    ui::SemanticNodeSnapshot snapshot;
+    snapshot.info = std::move(info);
+}
