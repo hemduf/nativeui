c2ec7fce956f02bd8e8160e809beea2073c7a768
hemduf <fduhem@gmail.com>
2026-09-11 15:29:19 +0200

    style(t038): expose TextInputStyle through umbrella header

--- include/nativeui/nativeui.hpp
+++ include/nativeui/nativeui.hpp
@@ -14,6 +14,7 @@
 #include <nativeui/slider_style.hpp>
 #include <nativeui/progress_style.hpp>
 #include <nativeui/toggle_style.hpp>
+#include <nativeui/text_input_style.hpp>
 #include <nativeui/path.hpp>
 #include <nativeui/resource.hpp>
 #include <nativeui/embedded_resource.hpp>
