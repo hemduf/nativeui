29a764dab5c3a34b3df5cb09f6d4a254accd7cd6
hemduf <fduhem@gmail.com>
2026-09-11 16:51:21 +0200

    build(t038): expose TextArea style to widgets header

--- include/nativeui/widgets.hpp
+++ include/nativeui/widgets.hpp
@@ -2,6 +2,7 @@
 
 #include <nativeui/virtual_list.hpp>
 #include <nativeui/text_input_style.hpp>
+#include <nativeui/text_area_style.hpp>
 
 #include <nativeui/detail/widgets_basic.inc>
 #include <nativeui/detail/widgets_text_input.inc>
