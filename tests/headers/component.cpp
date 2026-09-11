c4e870f67eb3a5d788d8de8d55fc894948f43841
hemduf <fduhem@gmail.com>
2026-09-11 13:59:53 +0200

    test(t050): compile inspector tree seam when enabled

--- tests/headers/component.cpp
+++ tests/headers/component.cpp
@@ -1,3 +1,16 @@
+#define NATIVEUI_ENABLE_INSPECTOR 1
+
 #include <nativeui/component.hpp>
+#include <nativeui/component_tree.hpp>
+#include <nativeui/inspector.hpp>
+
+#include <type_traits>
+#include <utility>
+
+static_assert(std::is_same_v<
+              decltype(std::declval<ui::Tree&>().inspector_snapshot()),
+              ui::debug::InspectorSnapshot>);
+static_assert(std::is_copy_constructible_v<ui::debug::InspectorSnapshot>);
+static_assert(std::is_copy_constructible_v<ui::debug::InspectorNode>);
 
 void nativeui_header_compile_component() {}
