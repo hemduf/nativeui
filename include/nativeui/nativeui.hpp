5a53a44252f0dcd10304005071ff0f1f3d4ad3d7
hemduf <fduhem@gmail.com>
2026-09-11 15:17:24 +0200

    style(t038): expose ToggleStyle through umbrella header

--- include/nativeui/nativeui.hpp
+++ include/nativeui/nativeui.hpp
@@ -13,6 +13,7 @@
 #include <nativeui/style.hpp>
 #include <nativeui/slider_style.hpp>
 #include <nativeui/progress_style.hpp>
+#include <nativeui/toggle_style.hpp>
 #include <nativeui/path.hpp>
 #include <nativeui/resource.hpp>
 #include <nativeui/embedded_resource.hpp>
