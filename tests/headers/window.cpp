6b28086315986ed440ddfe172a31e9b7d271007e
hemduf <fduhem@gmail.com>
2026-09-11 13:58:14 +0200

    test(t066): pin public window control surface

--- tests/headers/window.cpp
+++ tests/headers/window.cpp
@@ -1,5 +1,7 @@
 #include <nativeui/window.hpp>
 
+#include <functional>
+#include <optional>
 #include <string_view>
 #include <type_traits>
 #include <utility>
@@ -15,12 +17,26 @@ static_assert(std::is_same_v<decltype(std::declval<ui::Application&>().run()), i
 static_assert(std::is_same_v<decltype(std::declval<ui::Application&>().quit_requested()), bool>);
 static_assert(std::is_same_v<decltype(std::declval<const ui::Application&>().quit_policy()), ui::QuitPolicy>);
 
+static_assert(std::is_same_v<decltype(ui::WindowDesc{}.min_size), std::optional<ui::Size>>);
+static_assert(std::is_same_v<decltype(ui::WindowDesc{}.max_size), std::optional<ui::Size>>);
+static_assert(std::is_enum_v<ui::CloseDecision>);
+
 static_assert(!std::is_copy_constructible_v<ui::StandaloneWindow>);
 static_assert(!std::is_copy_assignable_v<ui::StandaloneWindow>);
 static_assert(!std::is_move_constructible_v<ui::StandaloneWindow>);
 static_assert(!std::is_move_assignable_v<ui::StandaloneWindow>);
 static_assert(std::is_constructible_v<ui::StandaloneWindow, ui::Application&, ui::UI&, ui::WindowDesc>);
 static_assert(std::is_same_v<decltype(std::declval<const ui::StandaloneWindow&>().valid()), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().set_title(std::string_view{})), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().show()), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().hide()), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().set_min_size(std::optional<ui::Size>{})), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().set_max_size(std::optional<ui::Size>{})), bool>);
+static_assert(std::is_same_v<decltype(std::declval<const ui::StandaloneWindow&>().is_closed()), bool>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().on_close_request(
+                                  std::function<ui::CloseDecision()>{})), void>);
+static_assert(std::is_same_v<decltype(std::declval<ui::StandaloneWindow&>().on_closed(
+                                  std::function<void()>{})), void>);
 
 static_assert(!std::is_copy_constructible_v<ui::EmbeddedView>);
 static_assert(!std::is_copy_assignable_v<ui::EmbeddedView>);
