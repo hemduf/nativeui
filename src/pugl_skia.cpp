8c6841e4d1e809c064e9858ec3a31fd5680cb1b1
hemduf <fduhem@gmail.com>
2026-09-11 07:18:31 +0200

    fix(t043): preserve current platform dispatcher wrapper

--- src/pugl_skia.cpp
+++ src/pugl_skia.cpp
@@ -1,7 +1,39 @@
+#include <nativeui/detail/dispatcher_owner.hpp>
+
+#if defined(__APPLE__)
+#  include <CoreFoundation/CFRunLoop.h>
+#elif defined(_WIN32)
+#  ifndef NOMINMAX
+#    define NOMINMAX
+#  endif
+#  include <windows.h>
+#elif defined(__linux__)
+#  include <X11/Xlib.h>
+#  include <poll.h>
+#  undef None
+#endif
+
+#include <algorithm>
+#include <cerrno>
+#include <climits>
+#include <cmath>
+#include <mutex>
+#include <vector>
+
 #include "detail/native_ime_bridge.h"
 #include "detail/pugl_skia_setup.inc"
 #include "detail/pugl_skia_show_policy.inc"
+
+} // namespace
+namespace detail {
+
 #include "detail/pugl_skia_view_a.inc"
 #include "detail/pugl_skia_view_b.inc"
+
+using ViewCore = detail::ViewCore;
+
 #include "detail/pugl_skia_windows.inc"
+
+} // namespace ui
+
 #include "detail/pugl_skia_t043.inc"
