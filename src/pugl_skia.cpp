f68a1eafface94306cc343f373f6c32f7299233a
hemduf <fduhem@gmail.com>
2026-09-12 10:22:18 +0200

    feat(t064): wire Windows desktop services factory

--- src/pugl_skia.cpp
+++ src/pugl_skia.cpp
@@ -23,6 +23,8 @@
 #include "detail/application_platform_state.hpp"
 #if defined(__APPLE__)
 #  include "detail/macos_desktop_services.hpp"
+#elif defined(_WIN32)
+#  include "detail/windows_desktop_services.hpp"
 #elif defined(__linux__)
 #  include "detail/linux_desktop_services.hpp"
 #endif
