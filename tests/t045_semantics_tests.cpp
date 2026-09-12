87ad6388e7de81a7dbc768d27a1c506d8ff5bf12
hemduf <fduhem@gmail.com>
2026-09-12 15:12:11 +0200

    test(t068): isolate Button semantic RED in widget suite

--- tests/t045_semantics_tests.cpp
+++ tests/t045_semantics_tests.cpp
@@ -1,6 +1,5 @@
 #include <nativeui/detail/semantic_snapshot.hpp>
 #include <nativeui/semantics.hpp>
-#include <nativeui/widgets.hpp>
 
 #include <cstdint>
 #include <cstdlib>
@@ -38,18 +37,6 @@ void semantic_role_and_action_contract() {
     T045_CHECK(info.supports(ui::SemanticAction::Activate));
     T045_CHECK(info.supports(ui::SemanticAction::Focus));
     T045_CHECK(!info.supports(ui::SemanticAction::SetValue));
-}
-
-void standard_button_semantics_follow_t045_contract() {
-    ui::detail::ButtonComponent button{"Apply", [] {}};
-    const auto info = button.semantics();
-
-    T045_CHECK(info.role == ui::SemanticRole::Button);
-    T045_CHECK(info.name == "Apply");
-    T045_CHECK(info.focusable);
-    T045_CHECK(info.supports(ui::SemanticAction::Activate));
-    T045_CHECK(info.supports(ui::SemanticAction::Focus));
-    T045_CHECK(!info.supports(ui::SemanticAction::Toggle));
 }
 
 void semantic_snapshot_owns_data() {
@@ -361,7 +348,6 @@ void semantic_snapshot_publisher_reuses_virtual_metadata_for_scalar_changes() {
 int main() {
     try {
         semantic_role_and_action_contract();
-        standard_button_semantics_follow_t045_contract();
         semantic_snapshot_owns_data();
         virtual_collection_is_lazy_and_metadata_shared();
         virtual_metadata_is_immutable_after_publication();
