bb8a6ee585391f18b69938d88bfa7583b9915429
hemduf <fduhem@gmail.com>
2026-09-16 22:20:59 +0200

    fix(t125): preserve split routing include chain

--- include/nativeui/component_tree.hpp
+++ include/nativeui/component_tree.hpp
@@ -73,8 +73,8 @@ private:
 #undef paint_invalidator
 #undef availability_invalidator
 #include <nativeui/detail/tree_focus.inc>
-#include <nativeui/detail/tree_t125_availability.inc>
 #include <nativeui/detail/tree_input.inc>
+#include <nativeui/detail/tree_t125_availability.inc>
 #include <nativeui/detail/tree_t125_semantic.inc>
 #include <nativeui/detail/tree_focus_group.inc>
 #define register_dynamic_node unsafe_register_dynamic_node
